ce8c750c4eda85a9e46e0fc4509d2f004489b8d9
Arthur <newartroll11@gmail.com>
2021-11-03 14:39:04 +0400

    feat:(step)

---
+++ src/components/staps/Staps.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Staps.module.css";
+import steps from "../../images/steps.png";
+
+const Staps = () => {
+  return (
+    <div className={styles.steps}>
+      <div className={styles.imageArea}>
+        <img alt="image" src={steps} />
+      </div>
+    </div>
+  );
+};
+
+export default Staps;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import Header from '../../components/header/Header';
-import Socialpart from '../../components/socialPart/SocialPart';
-import styles from './Home.module.css'
+import React from "react";
+import Header from "../../components/header/Header";
+import Socialpart from "../../components/socialPart/SocialPart";
+import Staps from "../../components/staps/Staps";
+// import styles from './Home.module.css'
 
 const Home = () => {
-    return (
-        <>
-            <Header />
-            <Socialpart />
-        </>
-    );
-}
+  return (
+    <>
+      <Header />
+      <Socialpart />
+      <Staps />
+    </>
+  );
+};
 
 export default Home;
