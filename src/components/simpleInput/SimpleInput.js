b1c726012b686a36987ca033229c30bfac06bea8
Arthur Ghazaryan <newartroll11@gmail.com>
2021-11-15 11:02:15 +0400

    feat:(settings_generals)

---
+++ src/components/simpleInput/SimpleInput.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function SimpleInput({ plac, WIDTH }) {
+  return (
+    <input
+      placeholder={plac}
+      style={{
+        border: "1px solid #F4F4F4",
+        boxSizing: "border-box",
+        borderRadius: 6,
+        height: 40,
+        width: WIDTH,
+        margin: "10px",
+        paddingLeft:15
+      }}
+    />
+  );
+}
--- src/components/userComp/Tabs.js
+++ src/components/userComp/Tabs.js
@@ -4,6 +4,9 @@ import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import down from "../../images/download.png";
+import SimpleInput from "../simpleInput/SimpleInput";
+import Simplebutton from "../simpleButton/SimpleButton";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -59,7 +62,102 @@ export default function BasicTabs() {
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
-        GENERAL
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "center",
+          }}
+        >
+          <h2>GENERAL SETTINGS </h2>
+          <p
+            style={{
+              fontWeight: 500,
+              fontSize: 12,
+              color: "#A09DA1",
+              marginLeft: 20,
+            }}
+          >
+            Manage your login details and password.
+          </p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              border: "1px solid #DDE1EB",
+              boxSizing: "border-box",
+              borderRadius: 6,
+              padding: 20,
+            }}
+          >
+            <h2 style={{ fontSize: 16, color: "#475761", textAlign: "center" }}>
+              Change Personal Information
+            </h2>
+            <p
+              style={{
+                color: "#A09DA1",
+                textAlign: "center",
+                marginBottom: 20,
+              }}
+            >
+              If you change email address, make sure to confirm the one after.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-around",
+                alignItems: "center",
+              }}
+            >
+              <img alt="download" src={down} />
+              <div style={{ display: "grid", gridTemplateColumns: "100%" }}>
+                <SimpleInput plac="Charles Hu" WIDTH="445px" />
+                <SimpleInput plac="kivi_nz@hotmail.com" WIDTH="445px" />
+              </div>
+            </div>
+            <div style={{ display: "flex", justifyContent: "flex-end" }}>
+              <Simplebutton COLOR="#fff" BG="#1DAAFF" text="SAVE CHANGES" />
+            </div>
+          </div>
+          <div
+            style={{
+              border: "1px solid #DDE1EB",
+              boxSizing: "border-box",
+              borderRadius: 6,
+              padding: 20,
+              marginLeft: 20,
+            }}
+          >
+            <h2 style={{ fontSize: 16, color: "#475761", textAlign: "center" }}>
+              Change Personal Information
+            </h2>
+            <p
+              style={{
+                color: "#A09DA1",
+                textAlign: "center",
+                marginBottom: 20,
+              }}
+            >
+              If you change email address, make sure to confirm the one after.
+            </p>
+            <div style={{ display: "grid", gridTemplateColumns: "100%" }}>
+              <SimpleInput plac="Charles Hu" WIDTH="500px" />
+              <div>
+                <SimpleInput plac="kivi_nz@hotmail.com" WIDTH="240px" />
+                <SimpleInput plac="kivi_nz@hotmail.com" WIDTH="240px" />
+              </div>
+            </div>
+            <div style={{ display: "flex", justifyContent: "flex-end" }}>
+              <Simplebutton COLOR="#fff" BG="#1DAAFF" text="SAVE CHANGES" />
+            </div>
+          </div>
+        </div>
       </TabPanel>
       <TabPanel value={value} index={1}>
         BILLING
