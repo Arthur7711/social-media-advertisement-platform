49853fd25b5719c2a505cea39c610b1dd7e5780c
Arthur <newartroll11@gmail.com>
2021-11-24 16:58:23 +0400

    feat:(dashboard top part search)

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -3,6 +3,8 @@ import PersistentDrawerLeft from "../../components/userComp/headerNav/HeaderNav"
 import styles from "./Dashboard.module.css";
 import fb from "../../images/bfb.png";
 import insta from "../../images/binsta.png";
+import Simplebutton from "../../components/simpleButton/SimpleButton";
+import Simpleselect from "../../components/simpleSelect/SimpleSelect";
 
 const Dashboard = () => {
   return (
@@ -14,6 +16,44 @@ const Dashboard = () => {
           <img width="18px" height="18px" alt="insta" src={insta} />
           <span> Instagram </span>
         </div>
+        <div className={styles.subInfo}>
+          <h5>ACCOUNT OVERVIEW</h5>
+          <Simplebutton
+            text="New"
+            COLOR="#fff"
+            BG="linear-gradient(92.21deg, #005792 6.65%, #04118A 106.65%)"
+            WIDTH="100px"
+          />
+          <span>Aee your data and compare them with each other.</span>
+        </div>
+        <div>{/* charts will be here */}</div>
+        <div className={styles.subInfo}>
+          <h5>ACCOUNT BREAKDOWN VIEW</h5>
+          <Simplebutton
+            text="New"
+            COLOR="#fff"
+            BG="linear-gradient(92.21deg, #005792 6.65%, #04118A 106.65%)"
+            WIDTH="100px"
+          />
+          <span>
+            See which age, gender, countries, regions and platforms are bringing
+            you the best results.
+          </span>
+        </div>
+        <main className={styles.choise}>
+          <div className={styles.age}>
+            <h3>BREAKDOWN</h3>
+            <select name="age">
+              <option value="Age">Age</option>
+              <option value="saab">Saab</option>
+              <option value="mercedes">Mercedes</option>
+              <option value="audi">Audi</option>
+            </select>
+          </div>
+          <Simpleselect title="CLICKS" COLOR="#A09DA1" BG="#F4F4F4" />
+          <Simpleselect title="COST PER CLICK" COLOR="#A09DA1" BG="#F4F4F4" />
+          <Simpleselect title="COST PER CLICK" COLOR="#A09DA1" BG="#F4F4F4" />
+        </main>
       </div>
     </PersistentDrawerLeft>
   );
